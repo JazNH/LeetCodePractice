72d24e154cd4368a8c09e5314842280ce7cb493e
jaz <jasdeep.s94@gmail.com>
2022-07-10 10:57:45 -0400

    committing ConcatenationOfArray test file not done

---
+++ src/Test/Java/com/company/code/ConcatenationOfArrayTest.java
@@ -0,0 +1,19 @@
+package com.company.code;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConcatenationOfArrayTest {
+
+//    @Test
+//    void getConcatenation() {
+//        ConcatenationOfArray c = new ConcatenationOfArray();
+//        int[] nums = {1,2,1};
+//        int[] expected = {1,2,1,1,2,1};
+//        System.out.println(nums);
+//        System.out.println(expected);
+//        int[] actual = c.getConcatenation(nums);
+//        assertEquals(expected, actual);
+//    }
+}
