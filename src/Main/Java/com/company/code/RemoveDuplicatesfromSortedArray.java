e0277c1c580a87fb306c6cfae3bb6bb0c4209e97
jaz <jasdeep.s94@gmail.com>
2022-10-27 18:42:48 -0400

    committing RemoveDuplicatesfromSortedArray

--- src/Main/Java/com/company/code/RemoveDuplicatesfromSortedArray.java
+++ src/Main/Java/com/company/code/RemoveDuplicatesfromSortedArray.java
@@ -7,7 +7,9 @@ public class RemoveDuplicatesfromSortedArray {
         int index = 1;
         //length - 1 so we dont get index out of bounce error
         for(int i=0; i<nums.length-1;i++) {
+            //if two numbers next to each other aren't the same
             if(nums[i] != nums[i+1]) {
+                //then place current number on the index we're on. Index should hold all unique elements
                 nums[index++] = nums[i + 1];
             }
         }
